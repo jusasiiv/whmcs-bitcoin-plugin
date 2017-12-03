53903aa5b04c5180299d5ccf5b17aa5a88f8ffc0
Juha Siivikko <juha@siivikko.fi>
2017-12-03 17:51:01 +0100

    Added refund and cancel functions

--- blockonomics_bitcoin_payments.php
+++ blockonomics_bitcoin_payments.php
@@ -6,7 +6,7 @@
  * @copyright   Blockonomics (https://blockonomics.com)
  */
 
-function blockonomics_merchantgateway_MetaData()
+function blockonomics_MetaData()
 {
 	return array(
 		'DisplayName' => 'Blockonomics Merchant Gateway Module',
@@ -16,9 +16,13 @@ function blockonomics_merchantgateway_MetaData()
 	);
 }
 
-function blockonomics_merchantgateway_config()
+function blockonomics_config()
 {
 		return array(
+				"name" => "Blockonomics Merchant Gateway",
+				"description" => "Bitcoin payment method",
+				"version" => "0.1",
+				"author" => "Blockonomics",
 				// the friendly display name for a payment gateway should be
 				// defined here for backwards compatibility
 				'FriendlyName' => array(
@@ -43,5 +47,126 @@ function blockonomics_merchantgateway_config()
 				),
 		);
 }
+
+function blockonomics_link($params)
+{
+    // Gateway Configuration Parameters
+    $title = $params['title'];
+    $apiKey = $params['apiKey'];
+    // Invoice Parameters
+    $invoiceId = $params['invoiceid'];
+    $description = $params["description"];
+    $amount = $params['amount'];
+    $currencyCode = $params['currency'];
+    // Client Parameters
+    $firstname = $params['clientdetails']['firstname'];
+    $lastname = $params['clientdetails']['lastname'];
+    $email = $params['clientdetails']['email'];
+    $address1 = $params['clientdetails']['address1'];
+    $address2 = $params['clientdetails']['address2'];
+    $city = $params['clientdetails']['city'];
+    $state = $params['clientdetails']['state'];
+    $postcode = $params['clientdetails']['postcode'];
+    $country = $params['clientdetails']['country'];
+    $phone = $params['clientdetails']['phonenumber'];
+    // System Parameters
+    $companyName = $params['companyname'];
+    $systemUrl = $params['systemurl'];
+    $returnUrl = $params['returnurl'];
+    $langPayNow = $params['langpaynow'];
+    $moduleDisplayName = $params['name'];
+    $moduleName = $params['paymentmethod'];
+    $whmcsVersion = $params['whmcsVersion'];
+    $url = 'https://www.demopaymentgateway.com/do.payment';
+    $postfields = array();
+    $postfields['username'] = $username;
+    $postfields['invoice_id'] = $invoiceId;
+    $postfields['description'] = $description;
+    $postfields['amount'] = $amount;
+    $postfields['currency'] = $currencyCode;
+    $postfields['first_name'] = $firstname;
+    $postfields['last_name'] = $lastname;
+    $postfields['email'] = $email;
+    $postfields['address1'] = $address1;
+    $postfields['address2'] = $address2;
+    $postfields['city'] = $city;
+    $postfields['state'] = $state;
+    $postfields['postcode'] = $postcode;
+    $postfields['country'] = $country;
+    $postfields['phone'] = $phone;
+    $postfields['callback_url'] = $systemUrl . '/modules/gateways/callback/' . $moduleName . '.php';
+    $postfields['return_url'] = $returnUrl;
+    $htmlOutput = '<form method="post" action="' . $url . '">';
+    foreach ($postfields as $k => $v) {
+        $htmlOutput .= '<input type="hidden" name="' . $k . '" value="' . urlencode($v) . '" />';
+    }
+    $htmlOutput .= '<input type="submit" value="' . $langPayNow . '" />';
+    $htmlOutput .= '</form>';
+    return $htmlOutput;
+}
+
+function blockonomics_refund($params)
+{
+    // Gateway Configuration Parameters
+    $title = $params['title'];
+    $apiKey = $params['apiKey'];
+    // Transaction Parameters
+    $transactionIdToRefund = $params['transid'];
+    $refundAmount = $params['amount'];
+    $currencyCode = $params['currency'];
+    // Client Parameters
+    $firstname = $params['clientdetails']['firstname'];
+    $lastname = $params['clientdetails']['lastname'];
+    $email = $params['clientdetails']['email'];
+    $address1 = $params['clientdetails']['address1'];
+    $address2 = $params['clientdetails']['address2'];
+    $city = $params['clientdetails']['city'];
+    $state = $params['clientdetails']['state'];
+    $postcode = $params['clientdetails']['postcode'];
+    $country = $params['clientdetails']['country'];
+    $phone = $params['clientdetails']['phonenumber'];
+    // System Parameters
+    $companyName = $params['companyname'];
+    $systemUrl = $params['systemurl'];
+    $langPayNow = $params['langpaynow'];
+    $moduleDisplayName = $params['name'];
+    $moduleName = $params['paymentmethod'];
+    $whmcsVersion = $params['whmcsVersion'];
+    // perform API call to initiate refund and interpret result
+    return array(
+        // 'success' if successful, otherwise 'declined', 'error' for failure
+        'status' => 'success',
+        // Data to be recorded in the gateway log - can be a string or array
+        'rawdata' => $responseData,
+        // Unique Transaction ID for the refund transaction
+        'transid' => $refundTransactionId,
+        // Optional fee amount for the fee value refunded
+        'fees' => $feeAmount,
+    );
+}
+
+function blockonomics_cancelSubscription($params)
+{
+    // Gateway Configuration Parameters
+    $title = $params['title'];
+    $apiKey = $params['apiKey'];
+    // Subscription Parameters
+    $subscriptionIdToCancel = $params['subscriptionID'];
+    // System Parameters
+    $companyName = $params['companyname'];
+    $systemUrl = $params['systemurl'];
+    $langPayNow = $params['langpaynow'];
+    $moduleDisplayName = $params['name'];
+    $moduleName = $params['paymentmethod'];
+    $whmcsVersion = $params['whmcsVersion'];
+    // perform API call to cancel subscription and interpret result
+    return array(
+        // 'success' if successful, any other value for failure
+        'status' => 'success',
+        // Data to be recorded in the gateway log - can be a string or array
+        'rawdata' => $responseData,
+    );
+}
+
 
 ?>
