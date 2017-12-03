b2e85f47477a2e203fcba8079ca4a6ff489c28dd
Juha Siivikko <juha@siivikko.fi>
2017-12-03 15:00:14 +0100

    Added config params

--- blockonomics_bitcoin_payments.php
+++ blockonomics_bitcoin_payments.php
@@ -1,13 +1,47 @@
 <?php
+/**
+ * Blockonomics payment model
+ *
+ * @author      Blockonomics
+ * @copyright   Blockonomics (https://blockonomics.com)
+ */
 
-function merchantgateway_MetaData()
-	{
+function blockonomics_merchantgateway_MetaData()
+{
 	return array(
 		'DisplayName' => 'Blockonomics Merchant Gateway Module',
 		'APIVersion' => '1.1',
 		'DisableLocalCreditCardInput' => true,
 		'TokenisedStorage' => false,
 	);
+}
+
+function blockonomics_merchantgateway_config()
+{
+		return array(
+				// the friendly display name for a payment gateway should be
+				// defined here for backwards compatibility
+				'FriendlyName' => array(
+						'Type' => 'System',
+						'Value' => 'Blockonomics Merchant Gateway Module',
+				),
+				// a text field type allows for single line text input
+				'title' => array(
+						'FriendlyName' => 'Title',
+						'Type' => 'text',
+						'Size' => '50',
+						'Default' => 'Bitcoin',
+						'Description' => 'Payment method title',
+				),
+				// a text field type allows for single line text input
+				'apiKey' => array(
+						'FriendlyName' => 'API Key',
+						'Type' => 'text',
+						'Size' => '50',
+						'Default' => '',
+						'Description' => 'Enter your API key here',
+				),
+		);
 }
 
 ?>
