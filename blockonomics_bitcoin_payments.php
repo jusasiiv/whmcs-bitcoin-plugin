f7843a0d0d870c2a56ee26796d1c9a03b6db4776
Juha Siivikko <juha@siivikko.fi>
2017-12-03 14:19:47 +0100

    Added core php file for addon

---
+++ blockonomics_bitcoin_payments.php
@@ -0,0 +1,13 @@
+<?php
+
+function merchantgateway_MetaData()
+{
+	return array(
+               'DisplayName' => 'Blockonomics Merchant Gateway Module',
+	       'APIVersion' => '1.1',
+	       'DisableLocalCreditCardInput' => true,
+	       'TokenisedStorage' => false,
+	);
+}
+
+?>
