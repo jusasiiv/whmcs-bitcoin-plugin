29f13e5dbd47ef2a0c9cf9db0f12f7fdefd4d623
Juha Siivikko <juha@siivikko.fi>
2017-12-17 16:40:23 +0100

    Moved address generation inside the Blockonomics class

--- modules/gateways/Blockonomics/Blockonomics.php
+++ modules/gateways/Blockonomics/Blockonomics.php
@@ -6,6 +6,10 @@ use WHMCS\Database\Capsule;
 
 class Blockonomics {
 
+	/*
+	 * Try to get callback secret from db
+	 * If no secret exists, create new
+	 */
 	public function getCallbackSecret() {
 
 		$api_secret = '';
@@ -27,6 +31,9 @@ class Blockonomics {
 		return $api_secret;
 	}
 
+	/*
+	 * Generate new callback secret using sha1, save it in db under tblpaymentgateways table
+	 */
 	private function generateCallbackSecret() {
 
 		try {
@@ -42,4 +49,40 @@ class Blockonomics {
 
 		return $callback_secret;
 	}
+
+	/*
+	 * Get new address from Blockonomics Api
+	 */
+	public function getNewBitcoinAddress() {
+		$api_key = Capsule::table('tblpaymentgateways')
+			->where('gateway', 'blockonomics')
+			->where('setting', 'ApiKey')
+			->value('value');
+
+		$secret = $this->getCallbackSecret();
+
+		// Secret is formatted http://url.com?secret=abc123,
+		// Get last 40 chars of the secret string
+		$secret = substr($secret, -40);
+
+		$options = [
+			'http' => [
+				'header'  => 'Authorization: Bearer ' . $api_key,
+				'method'  => 'POST',
+				'content' => ''
+			]
+		];
+
+		try {
+			$context = stream_context_create($options);
+			$separator = '?reset=1&';
+			$contents = file_get_contents('https://www.blockonomics.co/api/new_address'.$separator."match_callback=$secret", false, $context);
+			$new_address = json_decode($contents);
+		} catch (\Exception $e) {
+			echo "Error getting new address from Blockonomics!";;
+		}
+
+		return $new_address->address;
+	}
+
 }
--- payment.php
+++ payment.php
@@ -1,11 +1,16 @@
 <?php
 
+require_once(dirname(__FILE__) . '/modules/gateways/Blockonomics/Blockonomics.php');
+require __DIR__ . '/init.php';
+
 use WHMCS\ClientArea;
 use WHMCS\Database\Capsule;
+use Blockonomics\Blockonomics;
 
 define('CLIENTAREA', true);
 
-require __DIR__ . '/init.php';
+// Init Blockonomics class
+$blockonomics = new Blockonomics();
 
 $ca = new ClientArea();
 
@@ -16,32 +21,8 @@ $ca->addToBreadCrumb('payment.php', 'Blockonomics Bitcoin Payment');
 
 $ca->initPage();
 
-//$ca->requireLogin(); // Uncomment this line to require a login to access this page
-
-// Check login status
-if ($ca->isLoggedIn()) {
-
-	/**
-	 * User is logged in - put any code you like here
-	 *
-	 * Here's an example to get the currently logged in clients first name
-	 */
-
-	$clientName = Capsule::table('tblclients')
-		->where('id', '=', $ca->getUserID())->pluck('firstname');
-		// 'pluck' was renamed within WHMCS 7.0.  Replace it with 'value' instead.
-		// ->where('id', '=', $ca->getUserID())->value('firstname');
-	$ca->assign('clientname', $clientName);
-
-} else {
-
-	// User is not logged in
-	$ca->assign('clientname', 'Random User');
-
-}
-
 /***********************************************
- * POST PARAMETERS TO VARIABLES AND CHECK IF THEY EXIST
+ * SET POST PARAMETERS TO VARIABLES AND CHECK IF THEY EXIST
  */
 $fiat_amount = $_POST['price'];
 $currency = $_POST['currency'];
@@ -54,39 +35,9 @@ if(!$fiat_amount || !$currency || !$order_id) {
 /***********************************************
  * ADDRESS GENERATION
  */
-$api_key = Capsule::table('tblpaymentgateways')
-			->where('gateway', 'blockonomics')
-			->where('setting', 'ApiKey')
-			->value('value');
-
-$secret = Capsule::table('tblpaymentgateways')
-			->where('gateway', 'blockonomics')
-			->where('setting', 'ApiSecret')
-			->value('value');
-
-$secret = substr($secret, -40);
-
-$options = [
-	'http' => [
-		'header'  => 'Authorization: Bearer ' . $api_key,
-		'method'  => 'POST',
-		'content' => ''
-	]
-];
-
-try {
-	$context = stream_context_create($options);
-	$separator = '?reset=1&';
-	$contents = file_get_contents('https://www.blockonomics.co/api/new_address'.$separator."match_callback=$secret", false, $context);
-	$new_address = json_decode($contents);
-} catch (\Exception $e) {
-	echo "Error getting new address from Blockonomics!";;
-}
-
-$btc_address = $new_address->address;
+$btc_address = $blockonomics->getNewBitcoinAddress();
 
 $ca->assign('btc_address', $btc_address);
-$ca->assign('secret', $secret);
 
 /************************************************/
 
@@ -114,16 +65,8 @@ $ca->assign('order_id', $order_id);
  */
 Menu::addContext();
 
-/**
- * Setup the primary and secondary sidebars
- *
- * @link http://docs.whmcs.com/Editing_Client_Area_Menus#Context
- */
-//Menu::primarySidebar('announcementList');
-//Menu::secondarySidebar('announcementList');
 
 # Define the template filename to be used without the .tpl extension
-
 $ca->setTemplate('payment');
 
 $ca->output();
