6e323f7ab54027aa563bdb5e707652552ce9cbb7
Blockonomics <webmaster@blockonomics.co>
2019-07-15 15:59:23 +0530

    Updated error message text

--- modules/gateways/blockonomics.php
+++ modules/gateways/blockonomics.php
@@ -82,7 +82,7 @@ function blockonomics_config() {
 				} catch (err) {
 					var testSetupUrl = "$system_url" + "testSetup.php";
 					responseObj.error = true;
-					responseObj.errorStr = 'Unable to locate/execute ' + testSetupUrl + '. Contact blockonomics support for help';
+					responseObj.errorStr = 'Unable to locate/execute ' + testSetupUrl + '. Check your WHMCS System URL ';
 				}
 				if (responseObj.error) {
 					testSetupResultCell.innerHTML = "<label style='color:red;'>Error:</label> " + responseObj.errorStr + 
